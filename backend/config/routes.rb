693daffc9579cb22dcaf116aa57a504e006d53f4
Yesh Chandiramani <yesh0907@hotmail.com>
2017-06-17 19:01:09 +0800

    create infermedica api endpoint

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :show]
   resources :records, only: [:create, :update]
-  
+
+  post 'diagnosis/diagnose', 'diagnosis#diagnose'
+  post 'diagnosis/search/symptom', 'diagnosis#search_symptom'
+  post 'diagnosis/triage', 'diagnosis#triage'
+  post 'diagnosis/parse', 'diagnosis#parse'
+
   get 'scrape/ktph', 'scrape#ktph'
   get 'scrape/ttsh', 'scrape#ttsh'
   get 'scrape/shortest', 'scrape#shortest'
