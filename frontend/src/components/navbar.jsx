69514c35c66f7cc6339f10589b2a0eedc35e029b
ThunderLight25 <cogsteeler2@gmail.com>
2017-06-17 13:44:23 +0800

    Added components folder, react semantic-ui, react-router-redux

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Input, Menu, Segment } from 'semantic-ui-react' 
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <
+      </div>
+    );
+  }
+}
+
+export default Navbar;
